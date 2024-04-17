23f5ec7dcc0ecaad04044902db08a00d28375454
shabbir811 <uandi.ss8.ss@gmail.com>
2024-04-17 22:18:18 +0500

    add code at github

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+//take input from user to count words through inquirer
+import inquirer from "inquirer";
+
+
+let answer = await inquirer.prompt(
+    [
+        {
+            name: "sentence",
+            type: "input",
+            message: "enter sentence to count words"
+        }
+    ]
+)
+
+//get result from user and store into a variable and apply trim  and split method 
+let count = answer.sentence.trim().split(' ')
+
+// print user input 
+console.log(count);
+
+//print words count 
+console.log(`count of your sentence's words: ${count.length}`);
